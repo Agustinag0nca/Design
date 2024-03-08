6760ee47854293881badfd55ea42389e7ca1d1a9
Agustinag0nca <agustinagonca@gmail.com>
2024-03-07 21:41:29 -0300

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import NavBar from "./components/NavBar";
-function App() {
-  const [count, setCount] = useState(0)
+// client/App.js
+import { useState } from “react”
+import NavBar from “/components/NavBar.jsx”
 
+function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="App">
-      <NavBar/>
+        <NavBar />
       </div>
       <h1>Carrito de Compras</h1>
     </>
   );
 }
 
-const express = require('express');
-const app = express();
-
-app.use(express.static('public', {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.js') || path.endsWith('.jsx')) {
-      res.setHeader('Content-Type', 'text/javascript');
-    }
-  }
-}));
-
-app.listen(3000, () => {
-  console.log('Servidor en ejecución en el puerto 3000');
-});
-
-
-export default App
+export default App;
